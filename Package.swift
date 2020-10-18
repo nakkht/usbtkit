668b2e1a2a017475d0dc8fcef1b2990eabc5187e
Paulius Gudonis <pg@neqsoft.com>
2020-10-18 22:32:31 +0200

    Updated Package.swift with supported platforms Added Device struct definition

--- Package.swift
+++ Package.swift
@@ -4,16 +4,17 @@ import PackageDescription
 
 let package = Package(
     name: "UsbtKit",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13)
+    ],
     products: [
-        .library(
-            name: "UsbtKit",
-            targets: ["UsbtKit"]),
+        .library(name: "UsbtKit",
+                 targets: ["UsbtKit"]),
     ],
     targets: [
-        .target(
-            name: "UsbtKit"),
-        .testTarget(
-            name: "UsbtKitTests",
-            dependencies: ["UsbtKit"]),
+        .target(name: "UsbtKit"),
+        .testTarget(name: "UsbtKitTests",
+                    dependencies: ["UsbtKit"]),
     ]
 )
